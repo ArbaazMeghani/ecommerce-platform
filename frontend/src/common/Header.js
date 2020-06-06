9aa1040b4655b3b1df48bd7ec1322f41e81c5471
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-06 18:18:09 -0500

    Create Navigation area with cart button

--- frontend/src/common/Header.js
+++ frontend/src/common/Header.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/styles';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
 
-export default class Header extends React.Component {
+const styles = () => ({
+  root: {
+    flexGrow: 1,
+    marginLeft: "5%",
+    marginRight: "5%",
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
+class Header extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Header</h1>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              E-Commerce
+            </Typography>
+            <IconButton aria-label="Shopping Cart">
+              <ShoppingCart htmlColor="#FFFFFF"/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
 }
+
+export default withStyles(styles)(Header);
