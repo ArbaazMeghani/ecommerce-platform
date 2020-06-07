256a9c247c2c2d4b921264399f08bf11618a393c
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-07 16:20:13 -0500

    Update footer and add styling

--- frontend/src/common/Footer.js
+++ frontend/src/common/Footer.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { withStyles } from '@material-ui/styles';
+import { Typography } from '@material-ui/core';
 
-export default class Footer extends React.Component {
+const useStyles = () => ({
+  root: {
+    marginTop: "5%",
+    marginLeft: "5%",
+    marginRight: "5%",
+    marginBottom: "0",
+    borderTop: "solid",
+    borderColor: "DarkGrey",
+    paddingTop: "2%",
+    paddingBottom: "2%",
+    backgroundColor: "lavender",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
+
+class Footer extends React.Component {
   render() {
+    const {classes} = this.props
+    console.log(classes)
     return (
-      <div>
-        <h1> Footer </h1>
+      <div className={classes.root}>
+        <Typography>
+          © Sample.com
+        </Typography>
       </div>
     )
   }
 }
+
+export default withStyles(useStyles)(Footer);
