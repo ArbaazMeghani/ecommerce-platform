d8f0862b097d5c2550b550340991e8e8c5ca733f
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-07 11:34:49 -0500

    Create cart reducer to add and remove products from the cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
-import reducers from './reducers/reducers'
+import reducers from './reducers/Reducers'
 import { Provider } from 'react-redux'
 
 let store = createStore(reducers)
---
+++ frontend/src/reducers/CartReducer.js
@@ -0,0 +1,12 @@
+const cartReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD':
+      return [...state, action.payload]
+    case 'REMOVE':
+      return state.filter(product => product.productId != action.payload)
+    default:
+      return state
+  }
+}
+
+export default cartReducer
