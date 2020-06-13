9beab4dc9be85eb8c190d09f24e1c4baf162053c
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-12 19:48:34 -0500

    Add modal for cart

--- frontend/src/common/CartButton.js
+++ frontend/src/common/CartButton.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import { IconButton, Badge } from '@material-ui/core'
-import {useSelector} from 'react-redux'
+import { IconButton, Badge, Modal } from '@material-ui/core'
+import { useSelector } from 'react-redux'
 
 export default function CartButton() {
   const productCount = useSelector(state => state.cart.length)
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <IconButton aria-label="Shopping Cart">
-      <Badge badgeContent={productCount} color="secondary">
-        <ShoppingCart htmlColor="#FFFFFF"/>
-      </Badge>
-    </IconButton>
+    <>
+      <IconButton aria-label="Shopping Cart" onClick={handleOpen}>
+        <Badge badgeContent={productCount} color="secondary">
+          <ShoppingCart htmlColor="#FFFFFF"/>
+        </Badge>
+      </IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <h1>hello</h1>
+      </Modal>
+    </>
   )
 }
