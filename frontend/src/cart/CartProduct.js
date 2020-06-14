677420be403ad3fc2dbbfc0bc20af0304931360b
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-14 12:40:24 -0500

    Add checkout button to cart

--- frontend/src/cart/CartProduct.js
+++ frontend/src/cart/CartProduct.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Button } from '@material-ui/core'
 
 export default function CartProduct() {
   const products = useSelector(state => state.cart)
@@ -13,7 +14,12 @@ export default function CartProduct() {
   const allProducts = productList()
 
   if(allProducts.length > 0) {
-    return allProducts
+    return (
+      <>
+        {allProducts}
+        <Button variant="contained" color="primary">Checkout</Button>
+      </>
+    )
   } else {
     return <h3>Your Cart is Empty :(</h3>
   }
