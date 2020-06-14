96f4b6c1633d81bf5a0be2b21d2e9e45c35e948a
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-14 11:11:28 -0500

    Align modal center

--- frontend/src/common/CartButton.js
+++ frontend/src/common/CartButton.js
@@ -5,6 +5,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   paper: {
     position: 'absolute',
     width: 400,
@@ -38,6 +43,7 @@ export default function CartButton() {
       <Modal
         open={open}
         onClose={handleClose}
+        className={classes.modal}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
