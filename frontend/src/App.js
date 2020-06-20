c97e8a2981fcb3961d24414936de596461ce4518
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-19 19:30:58 -0500

    Add component for checkout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { Route } from 'react-router-dom/cjs/react-router-dom.min'
 import Header from './common/Header'
 import Footer from './common/Footer'
+import Checkout from './checkout/Checkout'
 
 class App extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class App extends React.Component {
           <Route exact path="/" component={Home} />
           <Route path="/test" render={() => (<h1>hi</h1>)} />
           <Route path="/product" component={ProductPage} />
+          <Route path="/checkout" component={Checkout} />
           <Footer />
         </div>
       </Router>
