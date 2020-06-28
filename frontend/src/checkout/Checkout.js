13420548f2f34aaf3025eae8480143ad5cdba560
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-06-28 17:03:05 -0500

    Add formatting for checkout page

--- frontend/src/checkout/Checkout.js
+++ frontend/src/checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { TextField, InputLabel, Select, FormControl } from '@material-ui/core'
+import { TextField, InputLabel, Select, FormControl, Grid } from '@material-ui/core'
 import StateChoices from './components/StateChoices'
 
 export default function Checkout() {
@@ -11,30 +11,47 @@ export default function Checkout() {
   })
 
   return (
-    <div>
+    <div style={{marginLeft: "20%", marginRight: "20%"}}>
       <h1>checkout</h1>
       <form>
-        <TextField id="standard-basic" label="First Name" />
-        <TextField id="standard-basic" label="Last Name" />
-        <br />
-        <TextField id="standard-basic" label="Email" />
-        <br />
-        <TextField id="standard-basic" label="Phone" />
-        <br />
-        <TextField id="standard-basic" label="Street Address" />
-        <br />
-        <TextField id="standard-basic" label="City" />
-        <FormControl>
-          <InputLabel id="demo-simple-select-label">State</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-          >
-            <StateChoices />
-          </Select>
-        </FormControl>
-        <TextField id="standard-basic" label="Zipcode" />
-
+        <Grid container spacing={0} justify="center" direction="column">
+        <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
+          <Grid item>
+            <TextField id="standard-basic" label="First Name" />
+          </Grid>
+          <Grid item>
+            <TextField id="standard-basic" label="Last Name" />
+          </Grid>
+          </Grid>
+          <Grid item>
+            <TextField id="standard-basic" label="Email" />
+          </Grid>
+          <Grid item>
+            <TextField id="standard-basic" label="Phone" />
+          </Grid>
+          <Grid item>
+            <TextField id="standard-basic" label="Street Address" />
+          </Grid>
+          <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
+            <Grid item>
+              <TextField id="standard-basic" label="City" />
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <InputLabel id="demo-simple-select-label">State</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                >
+                  <StateChoices />
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <TextField id="standard-basic" label="Zipcode" />
+            </Grid>
+          </Grid>
+        </Grid>
       </form>
       <h3>Total Price: {totalPrice}</h3>
     </div>
