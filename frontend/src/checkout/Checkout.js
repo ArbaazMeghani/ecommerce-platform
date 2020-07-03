32fb80f328775940aa5e2bb986f03908cf7fad90
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-07-02 20:06:18 -0500

    Submit order

--- frontend/src/checkout/Checkout.js
+++ frontend/src/checkout/Checkout.js
@@ -1,8 +1,8 @@
 import React, { useState }  from 'react'
 import { useSelector } from 'react-redux'
-import { InputLabel, Select, FormControl, Grid } from '@material-ui/core'
-import StateChoices from './components/StateChoices'
+import { Grid, Button } from '@material-ui/core'
 import CheckoutInput from './components/CheckoutInput'
+import StateDropdown from './components/StateDropdown'
 
 export default function Checkout() {
   const [userInfo, setUserInfo] = useState({
@@ -26,10 +26,16 @@ export default function Checkout() {
     setUserInfo({...userInfo, [field]: value})
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log("submitted")
+  }
+
   return (
     <div style={{marginLeft: "20%", marginRight: "20%"}}>
       <h1>checkout</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Grid container spacing={0} justify="center" direction="column">
           <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
             <CheckoutInput field="firstName" label="First Name" value={userInfo.firstName} updateValue={handleUpdate}/>
@@ -39,22 +45,13 @@ export default function Checkout() {
           <CheckoutInput field="phone" label="Phone" type="number" value={userInfo.phone} updateValue={handleUpdate}/>
           <CheckoutInput field="streetAddress" label="Street Address" value={userInfo.streetAddress} updateValue={handleUpdate}/>
           <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
-          <CheckoutInput field="city" label="City" value={userInfo.city} updateValue={handleUpdate}/>
-            <Grid item>
-              <FormControl>
-                <InputLabel id="demo-simple-select-label">State</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  required
-                >
-                  <StateChoices />
-                </Select>
-              </FormControl>
-            </Grid>
+            <CheckoutInput field="city" label="City" value={userInfo.city} updateValue={handleUpdate}/>
+            <StateDropdown field="state" value={userInfo.state} updateValue={handleUpdate}/>
             <CheckoutInput field="zipCode" label="Zipcode" value={userInfo.zipCode} updateValue={handleUpdate}/>
           </Grid>
         </Grid>
+        <br />
+        <Button variant="contained" type="submit" color="primary">Submit Order</Button>
       </form>
       <h3>Total Price: {totalPrice}</h3>
     </div>
---
+++ frontend/src/checkout/components/StateDropdown.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Grid, FormControl, Select, InputLabel, MenuItem } from '@material-ui/core'
+
+const states =[
+  "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", 
+  "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+  "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+  "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+  "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+]
+
+export default function StateDropdown({field, value, updateValue}) {
+  return (
+    <Grid item>
+      <FormControl required style={{width: "65px"}}>
+        <InputLabel id="demo-simple-select-label">State</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          required
+          value={value}
+          onChange={(event) => updateValue(field, event.target.value)}
+        >
+          {states.map(state => <MenuItem key={state} value={state}>{state}</MenuItem>)}
+        </Select>
+      </FormControl>
+    </Grid>
+  )
+}
