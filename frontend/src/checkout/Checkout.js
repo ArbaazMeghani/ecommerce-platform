164323942a99c9f67e7d838f31f895ef709bbf5b
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-07-25 12:16:08 -0500

    Move checkout form into its own component

--- frontend/src/checkout/Checkout.js
+++ frontend/src/checkout/Checkout.js
@@ -1,11 +1,9 @@
 import React, { useState }  from 'react'
 import { useSelector } from 'react-redux'
-import { Grid, Button } from '@material-ui/core'
-import CheckoutInput from './components/CheckoutInput'
-import StateDropdown from './components/StateDropdown'
 import { createOrder } from '../api'
-import {Elements, CardElement} from '@stripe/react-stripe-js'
+import {Elements, useElements, useStripe} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js'
+import CheckoutForm from './components/CheckoutForm'
 
 export default function Checkout() {
   const [userInfo, setUserInfo] = useState({
@@ -58,27 +56,8 @@ export default function Checkout() {
     <div style={{marginLeft: "20%", marginRight: "20%"}}>
       <h1>checkout</h1>
       <Elements stripe={stripePromise}>
-        <form onSubmit={handleSubmit}>
-          <Grid container spacing={0} justify="center" direction="column">
-            <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
-              <CheckoutInput field="firstName" label="First Name" value={userInfo.firstName} updateValue={handleUpdate}/>
-              <CheckoutInput field="lastName" label="Last Name" value={userInfo.lastName} updateValue={handleUpdate}/>
-            </Grid>
-            <CheckoutInput field="email" label="Email" value={userInfo.email} updateValue={handleUpdate}/>
-            <CheckoutInput field="phone" label="Phone" type="number" value={userInfo.phone} updateValue={handleUpdate}/>
-            <CheckoutInput field="streetAddress" label="Street Address" value={userInfo.streetAddress} updateValue={handleUpdate}/>
-            <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
-              <CheckoutInput field="city" label="City" value={userInfo.city} updateValue={handleUpdate}/>
-              <StateDropdown field="state" value={userInfo.state} updateValue={handleUpdate}/>
-              <CheckoutInput field="zipCode" label="Zipcode" value={userInfo.zipCode} updateValue={handleUpdate}/>
-            </Grid>
-          </Grid>
-          <CardElement />
-          <br />
-          <Button variant="contained" type="submit" color="primary">Submit Order</Button>
-        </form>
+        <CheckoutForm handleUpdate={handleUpdate} handleSubmit={handleSubmit} userInfo={userInfo} price={totalPrice}/>
       </Elements>
-      <h3>Total Price: {totalPrice}</h3>
     </div>
   )
 }
---
+++ frontend/src/checkout/components/CheckoutForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Grid, Button } from '@material-ui/core'
+import CheckoutInput from './CheckoutInput'
+import { CardElement } from '@stripe/react-stripe-js'
+import StateDropdown from './StateDropdown'
+
+const CheckoutForm = ({handleUpdate, handleSubmit, userInfo, price}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={0} justify="center" direction="column">
+        <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
+          <CheckoutInput field="firstName" label="First Name" value={userInfo.firstName} updateValue={handleUpdate}/>
+          <CheckoutInput field="lastName" label="Last Name" value={userInfo.lastName} updateValue={handleUpdate}/>
+        </Grid>
+        <CheckoutInput field="email" label="Email" value={userInfo.email} updateValue={handleUpdate}/>
+        <CheckoutInput field="phone" label="Phone" type="number" value={userInfo.phone} updateValue={handleUpdate}/>
+        <CheckoutInput field="streetAddress" label="Street Address" value={userInfo.streetAddress} updateValue={handleUpdate}/>
+        <Grid container spacing={2} justify="flex-start" alignItems="center" direction="row">
+          <CheckoutInput field="city" label="City" value={userInfo.city} updateValue={handleUpdate}/>
+          <StateDropdown field="state" value={userInfo.state} updateValue={handleUpdate}/>
+          <CheckoutInput field="zipCode" label="Zipcode" value={userInfo.zipCode} updateValue={handleUpdate}/>
+        </Grid>
+        <Grid item>
+          <CardElement />
+        </Grid>
+      </Grid>
+      <br />
+  <Button variant="contained" type="submit" color="primary">Pay ${price}</Button>
+    </form>
+  )
+}
+
+export default CheckoutForm
